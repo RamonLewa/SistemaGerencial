c360d6b63dac469a2b25d2674f9a3e505ef6646b
RamonLewa <ramonloewenstein@gmail.com>
2024-02-20 23:16:40 -0300

    Splash

--- SistemaGerencial/Program.cs
+++ SistemaGerencial/Program.cs
@@ -16,7 +16,7 @@ namespace SistemaGerencial
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Menu());
+            Application.Run(new Splash());
         }
     }
 }
---
+++ SistemaGerencial/Splash.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaGerencial
+{
+    public partial class Splash : Form
+    {
+        public Splash()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            panel2.Width += 7;
+
+            if (panel2.Width >= 599) 
+            {
+                timer1.Stop();
+                Menu menu = new Menu();
+                this.Hide();
+                menu.Show();
+            }
+        }
+    }
+}
